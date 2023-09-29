480c404c5d086fa8ea695423014ed65a5eb760f8
YuriyV86 <vyuriks@gmail.com>
2023-09-29 23:40:26 +0300

    calc

--- src/route/index.js
+++ src/route/index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 // Підключіть файли роутів
-const test = require('./test')
+const calc = require('./calc')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use('/calc', calc)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
